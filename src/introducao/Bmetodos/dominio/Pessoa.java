23250ce58f6de095d876a527545063eaaac5be4f
Ernandes <ernandes77@hotmail.com>
2023-05-10 19:24:50 -0300

    craindo classe Pessoa e Testando

---
+++ src/introducao/Bmetodos/dominio/Pessoa.java
@@ -0,0 +1,25 @@
+package introducao.Bmetodos.dominio;
+
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+
+    public void imprimi(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public int getIdade(){
+        return idade;
+    }
+}
---
+++ src/introducao/Bmetodos/teste/PessoaTeste.java
@@ -0,0 +1,16 @@
+package introducao.Bmetodos.teste;
+
+import introducao.Bmetodos.dominio.Pessoa;
+
+public class PessoaTeste {
+
+    public static void main(String[] args) {
+
+    Pessoa pessoa = new Pessoa();
+    pessoa.setNome("ernandes");
+    pessoa.setIdade(14);
+
+    pessoa.imprimi();
+
+    }
+}
