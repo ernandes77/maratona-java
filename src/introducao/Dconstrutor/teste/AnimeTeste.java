0fb1154c5f53b9a88b59645359b104992332e903
Ernandes <ernandes77@hotmail.com>
2023-05-11 19:36:43 -0300

    construtores

---
+++ src/introducao/Dconstrutor/teste/AnimeTeste.java
@@ -0,0 +1,14 @@
+package introducao.Dconstrutor.teste;
+
+
+import introducao.Dconstrutor.dominio.Anime;
+
+public class AnimeTeste {
+    public static void main(String[] args) {
+        Anime anime  = new Anime();
+        anime.init("goku","Luta",12,"açao");
+        anime.imprimi();
+    }
+
+
+}
