b1585fa1a87cf98693b1837454c9457161208057
Ernandes <ernandes77@hotmail.com>
2023-05-12 09:59:11 -0300

    bloco de inicializacao

---
+++ src/introducao/Eblocoinicializacao/dominio/Anime.java
@@ -0,0 +1,14 @@
+package introducao.Eblocoinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    
+    public Anime(){
+        System.out.println(episodios);
+
+    }
+    public int[] getEpisodios(){
+        return episodios;
+    }
+}
