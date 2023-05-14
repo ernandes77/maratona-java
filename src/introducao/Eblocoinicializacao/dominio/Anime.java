15b4ac7b2c4e4447a05b74d8a9cb36b808587deb
Ernandes <ernandes77@hotmail.com>
2023-05-13 22:11:48 -0300

    bloco de inicializacao

--- src/introducao/Eblocoinicializacao/dominio/Anime.java
+++ src/introducao/Eblocoinicializacao/dominio/Anime.java
@@ -2,10 +2,13 @@ package introducao.Eblocoinicializacao.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios;
+    private int[] episodios={1,2,3,4,5,6,7,8,9,1,11,12};//cria um array com 12 posicoes
     
-    public Anime(){
-        System.out.println(episodios);
+    public Anime(){//cria construtor onde ele chama as posicoes com foreach
+        for (int episodio:this.episodios
+             ) {
+    System.out.print(episodio +" ");
+        }
 
     }
     public int[] getEpisodios(){
