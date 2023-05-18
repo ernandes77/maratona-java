f687b344565b2d06f5c9317bf478b0b84ddefc34
Ernandes <ernandes77@hotmail.com>
2023-05-18 20:54:58 -0300

    Create CarroTeste01.java
    criando 3 objetos carro onde chamara os atributos dentro do contrutor

---
+++ src/introducao/Fmodificadorestatico/teste/CarroTeste01.java
@@ -0,0 +1,13 @@
+package introducao.Fmodificadorestatico.teste;
+
+import introducao.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        Carro c2 = new Carro();
+        Carro c3 = new Carro();
+
+
+    }
+}
