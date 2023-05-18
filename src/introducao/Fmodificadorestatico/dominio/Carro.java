61891904047d105dbc7312235841e4dbdd810c11
Ernandes <ernandes77@hotmail.com>
2023-05-18 20:51:40 -0300

    metodo imprimi
    metodo imprimi chamando os atributos com get e set

--- src/introducao/Fmodificadorestatico/dominio/Carro.java
+++ src/introducao/Fmodificadorestatico/dominio/Carro.java
@@ -3,5 +3,36 @@ package introducao.Fmodificadorestatico.dominio;
 public class Carro {
     private String nome;
     private int velocidadeMaxima;
-    private int velocidadeLimite ;
+    private int velocidadeLimite = 250;
+
+    public void imprimi(){
+        System.out.println("--------------");
+        System.out.println("Nome"+this.nome);
+        System.out.println("velocidadeMaxima"+this.velocidadeMaxima);
+        System.out.println("velocidadeLimite"+this.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(int velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public int getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(int velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
 }
