ba260694482558b84c2bef124359d207d39062db
Ernandes <ernandes77@hotmail.com>
2023-05-22 21:54:04 -0300

    associacao

--- src/introducao/Gassociacao/dominio/Jogador.java
+++ src/introducao/Gassociacao/dominio/Jogador.java
@@ -1,4 +1,22 @@
 package introducao.Gassociacao.dominio;
 
 public class Jogador {
+
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprimi(){
+        System.out.println(this.nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/introducao/Gassociacao/test/JogadorTeste01.java
+++ src/introducao/Gassociacao/test/JogadorTeste01.java
@@ -1,4 +1,23 @@
 package introducao.Gassociacao.test;
 
+import introducao.Gassociacao.dominio.Jogador;
+
 public class JogadorTeste01 {
+
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("ernandes");
+        Jogador jogador2 = new Jogador("pele");
+        Jogador jogador3 = new Jogador("evandro");
+
+        Jogador [] jogadores = {jogador1,jogador2,jogador3};
+        for (Jogador jogador : jogadores) {
+            jogador.imprimi();
+        }
+
+
+    }
+
+
+    }
+    }
 }
