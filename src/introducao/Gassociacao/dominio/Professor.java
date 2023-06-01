3ef8230325d9edb9df907925a0cd8e8c16976dc7
Ernandes <ernandes77@hotmail.com>
2023-05-31 22:30:26 -0300

    associacao teste

--- src/introducao/Gassociacao/dominio/Professor.java
+++ src/introducao/Gassociacao/dominio/Professor.java
@@ -3,9 +3,38 @@ package introducao.Gassociacao.dominio;
 public class Professor {
 
     private String nome;
+    private String especialidade;
+    private Seminario seminario;
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario seminario) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminario = seminario;
+    }
+
+    public Seminario getSeminario() {
+        return seminario;
+    }
+
+    public void setSeminario(Seminario seminario) {
+        this.seminario = seminario;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getNome() {
---
+++ src/introducao/Gassociacao/test/AssociacaoTeste01.java
@@ -0,0 +1,23 @@
+package introducao.Gassociacao.test;
+
+import introducao.Gassociacao.dominio.Aluno;
+import introducao.Gassociacao.dominio.Local;
+import introducao.Gassociacao.dominio.Professor;
+import introducao.Gassociacao.dominio.Seminario;
+
+public class AssociacaoTeste01 {
+    public static void main(String[] args) {
+        Local local = new Local("rua12");
+        Aluno aluno = new Aluno("ernandes",34);
+        Professor professor = new Professor("Diego","pirata");
+
+        Aluno[]alunosSeminario={aluno};
+
+        Seminario seminario = new Seminario("onde achar aula",alunosSeminario,local);
+
+        Seminario[]seminariosDisponiveis={seminario};
+
+        professor.setSeminario(seminariosDisponiveis);
+
+    }
+}
