2ff1d266a2f2eac1edcf78e9ad63b705129f7981
Ernandes <ernandes77@hotmail.com>
2023-06-15 18:20:25 -0300

    sobescrita

---
+++ src/introducao/Isobrescrita/dominio/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package introducao.Isobrescrita.dominio.teste;
+
+import introducao.Isobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Stone");
+        System.out.println(anime);
+    }
+}
