cee00138e07f87dd81bf0c0362b4b6b840c320a3
Ernandes <ernandes77@hotmail.com>
2023-06-23 16:11:39 -0300

    Modificador Final

---
+++ src/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,10 @@
+package Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE;
+
+    static {
+        VELOCIDADE_LIMITE = 250;
+    }
+}
---
+++ src/Jmodificadorfinal/dominio/teste/Teste01.java
@@ -0,0 +1,8 @@
+package Jmodificadorfinal.dominio.teste;
+
+import introducao.Fmodificadorestatico.dominio.Carro;
+
+public class Teste01 {
+    Carro carro = new Carro();
+    System.out.println(Carro.VELOCIDADE_LIMITE);
+}
